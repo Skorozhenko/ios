f850f6955407fbf9e554e31c335d7a2ebce46380
Skorozhenko <skoroliza@mail.ru>
2023-11-20 09:42:47 -0800

    Initial Commit

---
+++ ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Елизавета Скороженко on 20.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
